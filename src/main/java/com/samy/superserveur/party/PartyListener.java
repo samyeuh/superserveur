753a13194ba5da657f5d1cdab4ead5f1a4bb3678
Samy <samy.bouhamidi@efrei.net>
2024-11-13 13:06:09 +0100

    todo

--- src/main/java/com/samy/superserveur/party/PartyListener.java
+++ src/main/java/com/samy/superserveur/party/PartyListener.java
@@ -1,4 +1,14 @@
 package com.samy.superserveur.party;
 
-public class PartyListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PartyListener implements Listener {
+
+    @EventHandler
+    public void onLeaderJoin(PlayerJoinEvent event) {
+        //TODO: quand un joueur rejoint un jeu et qu'il est le leader, tout le monde se tp, sinon il se tp seul
+    }
 }
